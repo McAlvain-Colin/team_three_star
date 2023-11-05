9b341b27a6970f9a443a925c102e4066c834003d
huytran845 <huytrit@gmail.com>
2023-11-05 13:46:07 -0800

    Added about us and news component, and installed angular materials.
    Created and connected an about us and news component to the folder. Additionally, installed angular materials to start integrating external libraries and components.

--- cssi-web/src/app/app.module.ts
+++ cssi-web/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutUsComponent } from './aboutUs/about-us.component';
+import { NewsComponent } from './news/news.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+	AboutUsComponent,
+	NewsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
