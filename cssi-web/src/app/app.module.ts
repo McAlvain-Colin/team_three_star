69e3798f5123b8befa2243a02bf08144a6b5e9eb
huytran845 <huytrit@gmail.com>
2023-11-07 11:57:33 -0800

    Added a tool bar with the links for sign-in/log-in
    Included a tool bar with the title and when minimized, the webpage will allow a side menu.

--- cssi-web/src/app/app.module.ts
+++ cssi-web/src/app/app.module.ts
@@ -6,19 +6,31 @@ import { AppComponent } from './app.component';
 import { AboutUsComponent } from './aboutUs/about-us.component';
 import { NewsComponent } from './news/news.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToolBarComponent } from './tool-bar/tool-bar.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
     AppComponent,
-	AboutUsComponent,
-	NewsComponent
+    AboutUsComponent,
+    NewsComponent,
+    ToolBarComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
