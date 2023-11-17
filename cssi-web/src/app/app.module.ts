d57c7bcedf88b6520ea1d470fa272e3d5b48bb80
huytran845 <huytrit@gmail.com>
2023-11-16 17:08:10 -0800

    Routed the home page and login page, and changed look of login page
    Added the routing links for the home and login page so with angular routing, and also routed links in html for the nav bar. Lastly, updated the style of the login page to more accurately reflect complete version.

--- cssi-web/src/app/app.module.ts
+++ cssi-web/src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatButtonModule } from '@angular/material/button';
 import { ToolBarComponent } from './tool-bar/tool-bar.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { HomeComponent } from './home/home.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 
 @NgModule({
-  declarations: [AppComponent, SignupComponent],
+  declarations: [AppComponent, SignupComponent, HomeComponent, ForgotPasswordComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    MatButtonModule,
     LoginComponent,
     ToolBarComponent,
   ],
--- cssi-web/src/app/login/login.component.ts
+++ cssi-web/src/app/login/login.component.ts
@@ -11,6 +11,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSelectModule } from '@angular/material/select';
+import { MatCardModule } from '@angular/material/card';
 import { ToolBarComponent } from '../tool-bar/tool-bar.component';
 
 @Component({
@@ -23,6 +24,7 @@ import { ToolBarComponent } from '../tool-bar/tool-bar.component';
     MatFormFieldModule,
     MatInputModule,
     MatSelectModule,
+    MatCardModule,
     MatIconModule,
     MatButtonModule,
     FormsModule,
@@ -31,15 +33,32 @@ import { ToolBarComponent } from '../tool-bar/tool-bar.component';
   ],
 })
 export class LoginComponent {
-  email = new FormControl('', [Validators.required, Validators.email]);
-  hide = true;
+  emailField = new FormControl('', [Validators.required, Validators.email]);
+  hide: boolean = true;
+  name: string = '';
+  email: string = '';
+  password: string = '';
+
+  //use the `` to allow connections to the variable in the declaration.
+  submitForm() {
+    var message: string = `Welcome ${this.email}`;
+    if (
+      this.emailField.hasError('required') ||
+      this.emailField.hasError('email')
+    ) {
+      message = 'Email incorrect!';
+      alert(message);
+    } else {
+      alert(message);
+    }
+  }
 
   // This method gets an error message based on what error that the user has produced, empty, or invalid email.
   getErrorMessage() {
-    if (this.email.hasError('required')) {
+    if (this.emailField.hasError('required')) {
       return 'You must enter a value';
     }
 
-    return this.email.hasError('email') ? 'Not a valid email' : '';
+    return this.emailField.hasError('email') ? 'Not a valid email' : '';
   }
 }
