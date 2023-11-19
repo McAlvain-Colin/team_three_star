91e2e134f50ebb6e89d80e840510ceda71e74e7c
vargas-david <dvargasdelgado@nevada.unr.edu>
2023-11-19 14:33:34 -0800

    added vew stats /export data options and chart for data, input fields for time intervals, including device map

--- Device_Health/src/app/app.component.ts
+++ Device_Health/src/app/app.component.ts
@@ -1,18 +1,20 @@
+import { PercentPipe } from '@angular/common';
 import { Component } from '@angular/core';
 
 export interface DeviceElement {
   endDeviceId: number,
   appId: number,
-  packetLoss: number[]
-  
-  
+  packetLoss: number[],
+  gatewayId: string,
+  time: string[]
+  batteryStat: number[]
 };
 
 const ELEMENT_DATA: DeviceElement[] = 
 [
-  {endDeviceId: 123, appId: 456, packetLoss: [1.5, 2, 4, 2, 4, 2,1,2]},
-  {endDeviceId: 12, appId: 431, packetLoss: [1.7, 1, 3, 2, 4, 2,4,1]},
-  {endDeviceId: 143, appId: 464, packetLoss: [1,2,3,4,2,1,2,1]}
+  {endDeviceId: 123, appId: 456, packetLoss: [1.5, 2, 4], gatewayId: 'gtw1',  time: ['11/17/23 10:05', '11/18/23 12:15', '11/19/23 04:15'], batteryStat: [3,2,1] },
+  {endDeviceId: 120, appId: 431, packetLoss: [1.7, 1, 3], gatewayId: 'gtw3', time: ['11/16/23 10:05', '11/18/23 01:15', '11/19/23 04:15'], batteryStat: [3,2,1] },
+  {endDeviceId: 143, appId: 464, packetLoss: [1,2,3], gatewayId: 'gtw2', time: ['11/15/23 10:05', '11/18/23 11:15', '11/19/23 04:15'], batteryStat: [3,2,1] }
 ];
 
 @Component({
