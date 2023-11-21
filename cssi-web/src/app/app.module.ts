30bb966f8031ced677e7cd633c9439c8281f41bf
huytran845 <huytrit@gmail.com>
2023-11-21 14:35:49 -0800

    Added a dashboard nav bar for integration with the main dashboard of device data.
    The navigation bar added aligns with the elements from previous discussions within group meetings. Took into consideration the add and remove buttons being above the organization list. Also tweaked a bit of the home page for a bit more substance.

--- cssi-web/src/app/app.module.ts
+++ cssi-web/src/app/app.module.ts
@@ -11,6 +11,11 @@ import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { HomeComponent } from './home/home.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { DashboardNavComponent } from './dashboard-nav/dashboard-nav.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent],
@@ -23,6 +28,11 @@ import { ForgotPasswordComponent } from './forgot-password/forgot-password.compo
     SignupComponent,
     ForgotPasswordComponent,
     ToolBarComponent,
+    DashboardNavComponent,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
