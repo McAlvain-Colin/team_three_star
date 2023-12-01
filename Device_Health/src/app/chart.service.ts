c8988203222e8402a6e0ac356bc755c2c7c86c06
vargas-david <dvargasdelgado@nevada.unr.edu>
2023-12-01 00:53:28 -0800

    added functionality to view Packet loss nd view battery buttons

--- Device_Health/src/app/chart.service.ts
+++ Device_Health/src/app/chart.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Chart } from 'chart.js/auto';
 import { DeviceElement } from './app.component';
-import { startWith } from 'rxjs';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -10,52 +10,89 @@ export class ChartService {
   constructor() { }
 
   chart!: Chart;
+  chartLabels!: string[];
+  chartData!: number[];
 
-  createPktLossChart(device: DeviceElement): Chart
-  {
+  createPktLossChart(device: DeviceElement)
+  { 
+    this.chartData = device.packetLoss;
+    console.log('inside create pkt ');
+    //this.chart.destroy();
 
     this.chart = new Chart("myChart", 
-                    {
-                      type: 'line',
-                      data: {
-                        labels: device.time,
-                        datasets: [{
-                          label: 'packet loss',
-                          data: device.packetLoss
-                        }]
-                      }
-                    });
-    return this.chart;
+    {
+      type: 'line',
+      data: {
+        labels: device.time,
+        datasets: [{
+          label: 'Packet Loss',
+          data: device.packetLoss
+        }]
+          //removing this line will result in the chart reamining at the lowest window size that was opened
+      }, options: {maintainAspectRatio: false}
+    });
   }
 
-  updateChartData(row: DeviceElement)
+  updateChartData(row: DeviceElement, typeOfChart: string)
   {
-    this.chart.data.datasets[0].data = row.packetLoss;
-    this.chart.update();
+    if(row !== undefined)
+    {
+      if(typeOfChart === 'packetLoss')
+      {
+        this.chart.data.datasets[0].data = row.packetLoss;
+        
+      }
+      else if(typeOfChart === 'batteryStat')
+      {
+        this.chart.data.datasets[0].data = row.batteryStat;
+
+      }
+      this.chart.update();
+    }
+
   }
 
-  toBarChart(device: DeviceElement)
-  {
-   // this.chart.destroy();
-    this.chart = new Chart("myChart", {
-                            type: 'bar',
-                            data: {
-                              labels: device.time,
-                              datasets: [{
-                                //label: '# of Votes',
-                                data: device.batteryStat,
-                                borderWidth: 1
-                              }]
-                            },
-                            options: {
-                              scales: {
-                                y: {
-                                  beginAtZero: true
-                                }
-                              }
-                            }
-                          });
-   
+  createBarChart(device: DeviceElement)
+  {   
+    console.log('inside of createBarChart'); 
+    console.log(device); 
+    this.chartData = device.batteryStat;
+    console.log(this.chartData);
+
+    this.chart.destroy();
+    
+    this.chart = new Chart("myChart", 
+    {
+      type: 'bar',
+      data: {
+            labels: device.time,
+            datasets: [{
+              label: 'Battery',
+              data: device.batteryStat
+            }]
+          }, options: {maintainAspectRatio: false}
+    });
   }
 
+  createPktChart(device: DeviceElement)
+  {   
+    console.log('inside of createBarChart'); 
+    console.log(device); 
+    this.chartData = device.batteryStat;
+    console.log(this.chartData);
+
+    this.chart.destroy();
+    
+    this.chart = new Chart("myChart", 
+    {
+      type: 'line',
+      data: {
+            labels: device.time,
+            datasets: [{
+              label: 'Battery',
+              data: device.packetLoss
+            }]
+          }, options: {maintainAspectRatio: false}
+    });
+  }
 }
