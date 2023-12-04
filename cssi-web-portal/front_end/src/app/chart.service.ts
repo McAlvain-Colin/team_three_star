3758f18822bd08adfd0cafe3efdf680fef5e5ef5
vargas-david <dvargasdelgado@nevada.unr.edu>
2023-12-04 08:54:38 -0800

    added responsive design to device table(part of device health)

--- cssi-web-portal/front_end/src/app/chart.service.ts
+++ cssi-web-portal/front_end/src/app/chart.service.ts
@@ -10,6 +10,8 @@ export class ChartService {
   constructor() {}
 
   chart!: Chart;
+  chartLabels!: string[];
+  chartData!: number[];
 
   createPktLossChart(device: DeviceElement): Chart {
     this.chart = new Chart('myChart', {
--- cssi-web-portal/front_end/src/app/dashboard/dashboard.component.ts
+++ cssi-web-portal/front_end/src/app/dashboard/dashboard.component.ts
@@ -20,7 +20,30 @@ export interface DeviceElement {
 }
 
 const ELEMENT_DATA: DeviceElement[] = [
-  // Your data
+  {
+    endDeviceId: 123,
+    appId: 456,
+    packetLoss: [1.5, 2, 4],
+    gatewayId: 'gtw1',
+    time: ['11/17/23 10:05', '11/18/23 12:15', '11/19/23 04:15'],
+    batteryStat: [3, 2, 1],
+  },
+  {
+    endDeviceId: 120,
+    appId: 431,
+    packetLoss: [1.7, 1, 3],
+    gatewayId: 'gtw3',
+    time: ['11/16/23 10:05', '11/18/23 01:15', '11/19/23 04:15'],
+    batteryStat: [3, 2.5, 1],
+  },
+  {
+    endDeviceId: 143,
+    appId: 464,
+    packetLoss: [1, 2, 3],
+    gatewayId: 'gtw2',
+    time: ['11/15/23 10:05', '11/18/23 11:15', '11/19/23 04:15'],
+    batteryStat: [4, 2, 1.5],
+  },
 ];
 
 @Component({
