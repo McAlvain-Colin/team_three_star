4969fd96acc94eec77b20e9e85473121a4aca495
huytran845 <huytrit@gmail.com>
2023-12-15 19:49:38 -0800

    Added the rest of the comments for the remaining components I've edited.

--- cssi-web-portal/front_end/src/app/home/home.component.ts
+++ cssi-web-portal/front_end/src/app/home/home.component.ts
@@ -3,6 +3,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { ToolBarComponent } from '../tool-bar/tool-bar.component';
 import { TempNavBarComponent } from '../temp-nav-bar/temp-nav-bar.component';
 
+//Basic generation of the typescript by angular, and the imports were made by Huy to properly import the correct materials for the page.
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
