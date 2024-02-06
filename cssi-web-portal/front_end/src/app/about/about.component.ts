83bf61b228489cb162f6d46d6d010af5d973fb15
McAlvain_Colin <cmcalvain@nevada.unr.edu>
2024-02-05 22:33:37 -0800

    got the images in the about to display. I need to procure images of missing staff and advisors. Looking at unr website.

--- cssi-web-portal/front_end/src/app/about/about.component.ts
+++ cssi-web-portal/front_end/src/app/about/about.component.ts
@@ -5,6 +5,7 @@ import { MatCardModule } from '@angular/material/card';
 import { DashboardNavComponent } from '../dashboard-nav/dashboard-nav.component';
 import { TempNavBarComponent } from '../temp-nav-bar/temp-nav-bar.component';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { CommonModule } from '@angular/common';
 
 
 
@@ -19,8 +20,23 @@ import { MatGridListModule } from '@angular/material/grid-list';
     DashboardNavComponent,
     TempNavBarComponent,
     MatGridListModule,
+    CommonModule,
   ],
 })
 export class AboutComponent {
-  currentDate: Date = new Date();
+  team_images = [
+    { url: '../../assets/Colin.jpg', alt: 'Colin McAlvain'},
+    { url: '../../assets/David.jpg', alt: 'David Vargas'},
+    { url: '../../assets/Huy.jpg', alt: 'Huy Tran'},
+  ];
+  professor_images = [
+    { url: '../../assets/Colin.jpg', alt: 'David Feil-Seifer'},
+    { url: '../../assets/David.jpg', alt: 'Devrin Lee'},
+    { url: '../../assets/Huy.jpg', alt: 'Sara Davis'},
+  ];
+  advisor_image = [
+    { url: '../../assets/zach.png', alt: 'Dr. Dascalu '},
+    { url: '../../assets/zach.png', alt: 'Zach Estreito'},
+    { url: '../../assets/Colin.jpg', alt: 'Vinh Le'},
+  ];
 }
