77950835eeda42f17a2023ea91410f3fef39b4cb
McAlvain-Colin <cmcalvain@nevada.unr.edu>
2024-03-02 17:57:28 -0800

    added more debug statements. Error reported as 'the JSON object must be a str, bytes, or bytearray, not dict' tracking line error is in

--- cssi-web-portal/back_end/app/data_parser.py
+++ cssi-web-portal/back_end/app/data_parser.py
@@ -9,12 +9,15 @@ def parse_data(records):
     try:
         print('a')
         for row in records:
+            print('c')
             dev_eui, dev_time, payload, metadata = row
-
+            print('d')
             payload_dict = json.loads(payload)
+            print('e')
             metadata_dict = json.loads(metadata)
-            
-            dictionary_list.append((payload_dict, metadata_dict))  
+            print('f')
+            dictionary_list.append((payload_dict, metadata_dict)) 
+            print('g') 
             sensor_data_list.append({'dev_eui': dev_eui, 'dev_time': dev_time, 'payload_dict': payload_dict, 'metadata_dict': metadata_dict})
         print('b')        
         return sensor_data_list
