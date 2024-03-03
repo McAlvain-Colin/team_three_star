f086cf7820478f5ebfb74d010cff4048f77c7140
McAlvain-Colin <cmcalvain@nevada.unr.edu>
2024-03-02 18:42:14 -0800

    altered the json.loads to json.dumps in the data_parser to allow for dicts in place of strings after moving from csv implimentation to database calls.

--- cssi-web-portal/back_end/app/data_parser.py
+++ cssi-web-portal/back_end/app/data_parser.py
@@ -1,5 +1,47 @@
+#example of data that needs to be parsed
+#('A84041A215B5287D', datetime.datetime(2023, 7, 14, 20, 16, 17, 719000, tzinfo=datetime.timezone.utc), 
+# {'BatV': 3.668, 'Temp_Red': 24.3, 'Work_mode': 'DIS18B20', 'Temp_Black': 'NULL', 'Temp_White': 24.8, 'ALARM_status': 'FALSE'}, 
+# {'snr': 10.25, 'rssi': -49, 'timestamp': 1832982356, 'gateway_ids': ['eui': 'C0EE40FFFF2A6645'], 'gateway_id': 'lab-laird-ip67'}, 
+# {'received_at': '2023-07-14T20:16:17.842313331Z', 'channel_rssi': -49, 'uplink_token': 'ChwKGgoObGFibGFpcmRfaXA2NxIgCSDUoGGwlkdGPQY0W2bkqMqoMCMSqytVg=='})
+
+#lines broken into parts I need to account for:
+#(
+#'A84041A215B5287D', 
+# datetime.datetime(2023, 7, 14, 20, 16, 17, 719000, tzinfo=datetime.timezone.utc), 
+# {'BatV': 3.668, 'Temp_Red': 24.3, 'Work_mode': 'DIS18B20', 'Temp_Black': 'NULL', 'Temp_White': 24.8, 'ALARM_status': 'FALSE'}, 
+# {'snr': 10.25, 'rssi': -49, 'timestamp': 1832982356, 'gateway_ids': ['eui': 'C0EE40FFFF2A6645'], 'gateway_id': 'lab-laird-ip67'}, {'received_at': '2023-07-14T20:16:17.842313331Z', 'channel_rssi': -49, 'uplink_token': 'ChwKGgoObGFibGFpcmRfaXA2NxIgCSDUoGGwlkdGPQY0W2bkqMqoMCMSqytVg=='}
+#)
+
+
+
+# "0025CA000015EEEE",
+# "Thu, 28 Dec 2023 05:21:39 GMT",
+# {
+#   "batteryCapacity": "80-100%",
+#   "batteryIndex": 5,
+#   "messageType": 1,
+#   "temperature": 2.93
+# },
+# {
+#   "channel_rssi": -75,
+#   "gateway_ids": {
+#     "eui": "C0EE40FFFF2A6648"
+#   },
+#   "gateway_id": "field-laird-ip67"
+# },
+# "location": {
+#   "latitude": 6200,
+#   "latitude": 39.081507,
+#   "longitude": -120.158286,
+#   "source": "SOURCE_REGISTRY"
+# },
+# "received_at": "2023-12-28T05:21:39.931559170Z",
+# "rssi": -75,
+# "snr": 9.75,
+# "timestamp": 3016301502,
+# "uplink_token": "Chk4HAoQznLlbqQtbGfpcmtqoXA2NxIwI05A//8qZkQvrekngsaDAjjj7sSBhCtuvXaAyCwwNHM5Fc="
+
 import json
-from models import sensor_data  
 
 
 def parse_data(records):
@@ -12,9 +54,9 @@ def parse_data(records):
             print('c')
             dev_eui, dev_time, payload, metadata = row
             print('d')
-            payload_dict = json.loads(payload)
+            payload_dict = json.dumps(payload)
             print('e')
-            metadata_dict = json.loads(metadata)
+            metadata_dict = json.dumps(metadata)
             print('f')
             dictionary_list.append((payload_dict, metadata_dict)) 
             print('g') 
