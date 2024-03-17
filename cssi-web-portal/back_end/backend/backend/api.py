e591f7d1b1dc3840b51885af97265128c38d92a0
davidvargas <davidvargas@david-VM.myguest.virtualbox.org>
2024-03-17 12:45:31 -0700

    using Flask SQL Alchemy paginate to return pages in JSON format for frontend visualizations for creating and displaying user organzaitions

--- cssi-web-portal/back_end/backend/backend/api.py
+++ cssi-web-portal/back_end/backend/backend/api.py
@@ -1,10 +1,10 @@
 from flask import Flask, request, jsonify, url_for, make_response
 from flask_cors import CORS, cross_origin
-# import datetime
+import datetime
 # import dbinteractions
 from flask_mail import Mail, Message
 
-from flask_jwt_extended import (create_access_token, JWTManager, 
+from flask_jwt_extended import (create_access_token, JWTManager,
                                 jwt_required, get_jwt_identity,
                                 set_access_cookies, unset_jwt_cookies
                                 )
@@ -16,10 +16,11 @@ import bcrypt
 
 #db imports
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, types, text, LargeBinary
+from sqlalchemy import String, types, text, LargeBinary, ForeignKey
 
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, registry 
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, registry, relationship
 from typing_extensions import Annotated
+from typing import List
 
 
 #db things###########################
@@ -42,7 +43,7 @@ mail = Mail()
 app = Flask(__name__)
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Locomexican22@localhost/postgres'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:@localhost/postgres'
 db.init_app(app)
 
 
@@ -50,12 +51,12 @@ db.init_app(app)
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_SSL'] = True
-app.config['MAIL_USERNAME'] = 'davidadbdiel775@gmail.com' # ALTERED FOR PRIVACY 
-app.config['MAIL_PASSWORD'] = 'qxyo rdpj nltb drga'     # ALTERED FOR PRIVACY 
+app.config['MAIL_USERNAME'] = '5@gmail.com' # ALTERED FOR PRIVACY
+app.config['MAIL_PASSWORD'] = ''     # ALTERED FOR PRIVACY
 
 #added this line to specify where the JWT token is when requests with cookies are recieved
 # app.config['JWT_TOKEN_LOCATION'] = ['cookies', 'headers', 'json']
-app.config['JWT_SECRET_KEY'] = 'secret' # ALTERED FOR PRIVACY 
+app.config['JWT_SECRET_KEY'] = 'secret' # ALTERED FOR PRIVACY
 app.config['JWT_ACCESS_TOKEN_EXPIRES'] = datetime.timedelta(minutes = 20)
 CORS(app, resources={r'*': {'origins': 'http://localhost:4200'}})
 
@@ -71,20 +72,51 @@ s = URLSafeTimedSerializer('email secret')
 class Account(Base):
     __tablename__ = "Account"
 
-    id:Mapped[int] = mapped_column(primary_key= True) #implicitly Serail datatype in Postgres  db 
+    id:Mapped[int] = mapped_column(primary_key= True) #implicitly Serail datatype in Postgres  db
     email:Mapped[str] = mapped_column(unique= True)
-    password:Mapped[bytes] = mapped_column(types.LargeBinary(), unique= True)
-    # salt:Mapped[bytes] = mapped_column(types.LargeBinary(), unique= True)
+    password:Mapped[bytes] = mapped_column(types.LargeBinary())
     verified: Mapped[bool] = mapped_column(unique= False)
+
+    orgAccounts: Mapped[List['OrgAccount']] = relationship(back_populates='account')
 
     def __init__(self, email, password, verified):
         self.email = email
         self.password = password
-        # self.salt = salt
         self.verified = verified
 
     def __repr__(self):
         return f'(id = {self.id}), salt = {self.salt}, email = {self.email}'
+   
+
+class Organization(Base):
+    __tablename__ = "Organization"
+
+    id:Mapped[int] = mapped_column(primary_key= True) #implicitly Serail datatype in Postgres  db
+    name: Mapped[str] = mapped_column(nullable= False)
+    description:Mapped[str] = mapped_column(nullable= True)
+
+    orgAccounts: Mapped[List['OrgAccount']] = relationship(back_populates='org')
+
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return f'organization: {self.name}'
+   
+
+class OrgAccount(Base):
+    __tablename__ = 'OrgAccount'
+
+    id:Mapped[int] = mapped_column(primary_key= True) #implicitly Serail datatype in Postgres  db
+
+    a_id: Mapped[int] = mapped_column(ForeignKey('Account.id'))
+    account: Mapped['Account'] = relationship(back_populates='orgAccounts')
+
+    o_id: Mapped[int] = mapped_column(ForeignKey('Organization.id'))
+    org: Mapped['Organization'] = relationship(back_populates='orgAccounts')
+
+   
 ###############################
 
 @app.route('/')
@@ -93,18 +125,18 @@ def index():
 
 #using flask JWT extended based on the example provided in docs using JWT tokens.
 @app.route('/login', methods = ['POST'])
-@cross_origin()
+# @cross_origin()
 def login_user():
 
     data = request.get_json()
     email = data['email']
     password =  data['password']
 
-    user = db.session.execute(db.select(Account).filter_by(email = email).scalar())
+    user = db.session.execute(db.select(Account).filter_by(email = email)).scalar()
 
     if bcrypt.checkpw(password.encode('utf-8'), user.password): #database logic for searching goes here
-        
-        token = create_access_token(identity = email)
+       
+        token = create_access_token(identity = user.id)
         return make_response(jsonify({'login': True, 'token': token}), 200)
 
     else:
@@ -112,14 +144,19 @@ def login_user():
 
 
 #used to test authorized routes, only authenticated users can get this info
-@app.route('/protected', methods = ['GET'])   
+@app.route('/protected', methods = ['GET'])  
 @jwt_required()
 def protected():
-    currentUser = get_jwt_identity()
-    return jsonify({'pro': 'tected'}), 200
+    # currentUser = get_jwt_identity()
+    page = db.paginate(db.select(Account))
+    j = [{'id': r.id,
+          'email': r.email
+            } for r in page.items
+        ]
+    return jsonify(j), 200
 
 
-@app.route('/createUser', methods = ['PUT'])   
+@app.route('/createUser', methods = ['PUT'])  
 def create_user():
 
     data = request.get_json()
@@ -128,14 +165,14 @@ def create_user():
 
 
     hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
-    
+   
     emailtoken = s.dumps(email, salt='email-confirm')
 
     # dbinteractions.createMember(email, password, False, bcrypt)
     newUser = Account(email, hashed, False)
     db.session.add(newUser)
     db.session.commit()
-    
+   
 
     msg = Message('Confirm Email', sender='davidadbdiel775@gmail.com', recipients= [email])
 
@@ -147,12 +184,12 @@ def create_user():
 
     return jsonify({'emailConfirmation': True})
 
-@app.route('/confirm_email/<token>')   
+@app.route('/confirm_email/<token>')  
 def confirm_email(token):
 
     try:
         email = s.loads(token, salt='email-confirm', max_age = 360)
-    
+   
         # dbinteractions.verifiyMember(email)
         newUser = db.session.execute(db.select(Account).filter_by(email = email)).scalar()
         newUser.verified = True
@@ -172,7 +209,7 @@ def confirm_email(token):
 
 
 
-@app.route('/logout', methods = ['DELETE'])   
+@app.route('/logout', methods = ['DELETE'])  
 @jwt_required()
 def logout_user():
 
@@ -183,21 +220,82 @@ def logout_user():
 
 
 
-@app.route('/deleteUser', methods = ['DELETE'])   
+@app.route('/deleteUser', methods = ['DELETE'])  
 @jwt_required()
 def deleteUser():
     # data = request.get_json()
     #remove user to database code
 
-    #need to add revoked token to revoked list 
+    #need to add revoked token to revoked list
 
 
     return jsonify(deleted_user =  True)
 
 
 
+@app.route('/createOrg', methods = ['POST'])  
+@jwt_required()
+def createOrganization():
+
+    data = request.get_json() #uid, org titel, org descritpion
+    userId = data['uid']
+    orgName = data['orgName']
+    descript = data['orgDescript']
+
+    #database code
+    newOrg = Organization(orgName, descript)
+
+    user = db.session.execute(db.select(Account).filter_by(id = userId)).scalar()
+
+
+    #link the account with the org
+    orgAcc = OrgAccount(account= user, org= newOrg)
+
+    db.session.add(newOrg)
+    db.session.commit()
+    db.session.add(orgAcc)
+    db.session.commit()
+   
+
+
+    return jsonify(orgCreated = True)
+
+
+
+@app.route('/userOrgList', methods = ['GET'])  
+def getOrgList():
+
+    uid = get_jwt_identity()
+    data = request.get_json()
+    pageNum = data['pageNum']
+    # currPage = data['currPage']
+
+    if pageNum <= db.paginate(db.select(Organization).join(Organization.orgAccounts).filter_by(a_id = uid), per_page= 20).pages:
+        try:
+            page = db.paginate(db.select(Organization).join(Organization.orgAccounts).filter_by(a_id = uid), page= pageNum, per_page= 20)
+            res = {
+                'currentPage': pageNum,
+                'pageTotal': page.pages,
+                'list': [
+                            {
+                                'name': p.name,
+                                'description': p.description
+                            } for p in page.items
+                        ]
+            }
+
+            return make_response(res, 200)
+       
+        except Exception as e:
+
+            return make_response({'error': str(e)}, 404)
+       
+    else:
+        return make_response({'error': "page doesn't exist"})
+
 
 if __name__ == '__main__':
     app.run(debug = True)
 
 #
+
