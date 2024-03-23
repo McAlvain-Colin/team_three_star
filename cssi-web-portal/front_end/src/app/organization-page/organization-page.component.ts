8e38020ac190407d8a33fbad5451702bc1c8703a
huytran845 <huytrit@gmail.com>
2024-03-22 23:16:57 -0700

    removed notification box due to change to email based, and added new org box for admin to deal with members, also fixed some css for most pages for proper background display

--- cssi-web-portal/front_end/src/app/organization-page/organization-page.component.ts
+++ cssi-web-portal/front_end/src/app/organization-page/organization-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { ChangeDetectorRef, Component, inject, ViewChild } from '@angular/core';
 import { DataLayout } from '../data.config';
 import { RequestService } from '../request.service';
 import { MatCardModule } from '@angular/material/card';
@@ -16,6 +16,16 @@ import { Observable, map, shareReplay } from 'rxjs';
 import { MatTabsModule } from '@angular/material/tabs';
 import { RemovalDialogComponent } from '../removal-dialog/removal-dialog.component';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import {
+  MatPaginator,
+  MatPaginatorIntl,
+  MatPaginatorModule,
+  PageEvent,
+} from '@angular/material/paginator';
+import {
+  _MatTableDataSource,
+  MatTableDataSource,
+} from '@angular/material/table';
 
 @Component({
   selector: 'app-organization-page',
@@ -29,6 +39,7 @@ import { MatDialog, MatDialogModule } from '@angular/material/dialog';
     MatIconModule,
     MatSidenavModule,
     MatListModule,
+    MatPaginatorModule,
     MatCardModule,
     MatTabsModule,
     MatButtonModule,
@@ -39,44 +50,32 @@ import { MatDialog, MatDialogModule } from '@angular/material/dialog';
   ],
 })
 export class OrganizationPageComponent {
-  //   //Here we're injecting the service into the component's constructor so that we can call the service's methods
-  //   constructor(private requestService: RequestService) {}
-  //   config: DataLayout | undefined;
-  //   headers: Array<string> | undefined;
-  //   //Broadcasts a data parameter with the Config interface layout, and then maps it into values for the object in this class
-  //   //Subscribe is a method of the HTTPClient which allows us to make an HTTP request for the backend
-  //   showData() {
-  //     this.requestService
-  //       .getJson()
-  //       //Calls the method to locate the Url to get the data from, then submits an HTTP Request where we clone the data over to config since they have the same type using destructuring
-  //       .subscribe((data) => (this.config = { ...data }));
-  //   }
-  //   showServerResponse() {
-  //     this.requestService
-  //       .getResponse()
-  //       //This subscription asks the server for a response, and the casts that response into the attributes in the HTTP Response class
-  //       .subscribe((response) => {
-  //         //Assigning keys from the values received from the the HTTP Request as well as the headers
-  //         const keys = response.headers.keys();
-  //         this.headers = keys.map(
-  //           (key) => '${key}: ${response.headers.get(key)}'
-  //         );
-  //         //Accesses the body of the response which has the typing as DataLayout
-  //         this.config = { ...response.body! };
-  //       });
-  //   }
-  //   newData() {
-  //     //this.requestService.sendData(newData); //Sends a newData variable to the sendData function to be processed
-  //   }
-
   orgId: number = 0;
   routerLinkVariable = '/hi';
   applications: string[] = [];
+  members: string[] = [];
   orgName: string | null = 'Cat Chairs';
   orgDescription: string | null =
     'This cat is the sole representative of this company, he watches over the data. If there are any issues that you have with this CEO, please send a request at 1-420-MEOW-MEOW.';
   imgName: string | null = 'placeholder_cat2';
   removeApps: boolean = false;
+  removeMembers: boolean = false;
+  isAdmin: boolean = true;
+  currentPage: number = 0;
+  appsSource = new MatTableDataSource(this.applications);
+  membersSource = new MatTableDataSource(this.members);
+
+  @ViewChild('appsPaginator', { static: true })
+  appsPaginator: MatPaginator = new MatPaginator(
+    new MatPaginatorIntl(),
+    ChangeDetectorRef.prototype
+  );
+
+  @ViewChild('membersPaginator', { static: true })
+  membersPaginator: MatPaginator = new MatPaginator(
+    new MatPaginatorIntl(),
+    ChangeDetectorRef.prototype
+  );
 
   constructor(private route: ActivatedRoute, public dialog: MatDialog) {} //makes an instance of the router
   ngOnInit(): void {
@@ -85,7 +84,9 @@ export class OrganizationPageComponent {
     if (this.orgName == null) {
       this.orgName = 'Cat Chairs';
     }
-    this.setupDevices();
+    this.setupLists();
+    this.appsSource.paginator = this.appsPaginator;
+    this.membersSource.paginator = this.membersPaginator;
   }
 
   private breakpointObserver = inject(BreakpointObserver);
@@ -97,6 +98,10 @@ export class OrganizationPageComponent {
       shareReplay()
     );
 
+  handlePageEvent(pageEvent: PageEvent, pageType: number) {
+    //Make get request here that sends in pageEvent.pageIndex
+  }
+
   confirmRemoval(itemName: string) {
     //SHould open a snackbar that asks if you want to remove the component, and then based on the action does the thing
     const removalDialogRef = this.dialog.open(RemovalDialogComponent, {
@@ -104,14 +109,15 @@ export class OrganizationPageComponent {
     });
   }
 
-  getRouteName(itemName: string) {
+  getRouteName(itemName: string, itemType: number) {
     let routeName: string = '/application/' + itemName;
     return routeName;
   }
 
-  setupDevices() {
+  setupLists() {
     for (let i = 1; i <= 10; i++) {
       this.applications.push('Application ' + i);
+      this.members.push('Member ' + i);
     }
   }
 }
